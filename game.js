ba6daabcf5f8d48569556a908939f942c2c0fa77
_akash.jana__ <102944323+akashjana2123@users.noreply.github.com>
2024-04-21 16:01:40 +0530

    Update game.js
    auto start

--- game.js
+++ game.js
@@ -4,11 +4,11 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-var started = false;
+var started = true;
 var level = 0;
 
 $(document).keypress(function() {
-  if (!started) {
+  if (started) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
